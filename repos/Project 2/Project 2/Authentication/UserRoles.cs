a69c2cfe0c33cb12f38212afaec8d46e933dc9b9
ThomsonMoco <110563213+ThomsonMoco@users.noreply.github.com>
2022-09-08 17:28:52 +0200

    Add files via upload

---
+++ repos/Project 2/Project 2/Authentication/UserRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_2.Authentication
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+    }
+}
